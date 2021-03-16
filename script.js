610d4e07ea22de45b4e5af1eff82c9726098d725
AlexanderA28 <58190710+AlexanderA28@users.noreply.github.com>
2021-03-16 20:20:42 +0000

    dynamic navbar

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-function myFunction() {
+function myFunction(clicked_id) {
 
     var base_url = "http://127.0.0.1:5500/"
-    var page = document.getElementById("myBtn").value
+    var page = document.getElementById(clicked_id).value
 
     var full_path = base_url.concat(page,'.html')
     
-    document.getElementById("myBtn").onclick = window.location.href = full_path;
+    document.getElementById(clicked_id).onclick = window.location.href = full_path;
 }
